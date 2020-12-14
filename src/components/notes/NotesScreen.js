d63e364752cd0b3fac97f860f31f2f67a79f9a01
EzequielCaste <ezequiel.caste@gmail.com>
2020-12-14 19:48:35 -0300

    Create the NoteScreen component and add styles

--- src/components/notes/NotesScreen.js
+++ src/components/notes/NotesScreen.js
@@ -7,6 +7,24 @@ export const NotesScreen = () => {
     <div className="notes__main-content">
 
       <NotesAppBar />
+
+      <div className="notes__content">
+        <input
+          autoComplete="off"
+          type="text"
+          className="notes__title-input"
+          placeholder="Some Text"
+        />
+
+        <textarea
+          placeholder="What happened today?"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Gloomy_Forest.jpg/275px-Gloomy_Forest.jpg" alt="forest" />
+        </div>
+      </div>
     </div>
   )
 }
