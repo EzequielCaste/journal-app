7449fa577f9dd359fec919126e280b3fda4a7335
EzequielCaste <ezequiel.caste@gmail.com>
2020-12-14 19:23:08 -0300

    NoteScreen component to show notes, add styles.

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Sidebar } from './Sidebar'
+//import { NoSelection } from '../notes/NoSelection'
+import { NotesScreen } from '../notes/NotesScreen'
 
 export const JournalScreen = () => {
   return (
-    <div className="journal__main-content">      
+    <div className="journal__main-content">
       <Sidebar />
       <main>
-        <h1>Journal Screen</h1>
+        {/* <NoSelection /> */}
+        <NotesScreen />
       </main>
     </div>
   )
