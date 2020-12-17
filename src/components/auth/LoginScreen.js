a5e25995adb835e99d4b04b0a9645f4b40fe90f3
EzequielCaste <ezequiel.caste@gmail.com>
2020-12-17 11:44:09 -0300

    Configure Firebase and Google Sign In

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { login, startLoginEmailPassword } from '../../actions/auth';
+import { startLoginEmailPassword, startGoogleLogin } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
@@ -17,6 +17,10 @@ export const LoginScreen = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     dispatch(startLoginEmailPassword(email, password))
+  }
+
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin(email, password))
   }
 
   return (
@@ -48,7 +52,10 @@ export const LoginScreen = () => {
           <p>
             Login with Social Networks
           </p>
-          <div className="google-btn">
+          <div
+            onClick={handleGoogleLogin}
+            className="google-btn"
+          >
             <div className="google-icon-wrapper">
               <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
             </div>
